dbd2b768de7331d4f27380914048bb3c8ce57089
Martin Helfer <martin.helfer@ub.uni-freiburg.de>
2022-02-16 21:19:32 +0100

    create only missing readfiles

--- createReadFiles.js
+++ createReadFiles.js
@@ -29,15 +29,19 @@ for (const jsonFile of jsonFiles) {
         const filename = topFolder + "-" + subFolder + "-" + jsonObj.file + ".wav";
 
         //Bobo Siebenschläfer - Willkommen im Kindergarten
+        //TODO: Autorälli, etc.
         const titleToRead = jsonObj.name.replace(/ \- \d+ \-/, "");;
-        console.log(titleToRead);
+        //console.log(titleToRead);
         const lang = jsonObj.lang || "de-DE";
 
-        //Sprachdatei berechnen, normalisieren und in Nextcloud abzlegn
-        const pico2waveTTScommand = `
+        //Fehlende Sprachdatei berechnen, normalisieren und in Nextcloud ablegen
+        if (!fs.existsSync(readFilesDir + "/" + filename)) {
+            console.log("create " + filename);
+            const pico2waveTTScommand = `
                                 pico2wave -l ${lang} -w ${__dirname}/tts.wav "${titleToRead}" &&
                                 ffmpeg -i ${__dirname}/tts.wav -af equalizer=f=300:t=h:width=200:g=-30 ${__dirname}/tts-eq.wav -hide_banner -loglevel error -y &&
                                 ffmpeg -i ${__dirname}/tts-eq.wav -af acompressor=threshold=-11dB:ratio=9:attack=200:release=1000:makeup=8 ${readFilesDir}/${filename} -hide_banner -loglevel error -y                            `;
-        execSync(pico2waveTTScommand);
+            execSync(pico2waveTTScommand);
+        }
     }
 }
