3860063d8da1887c487ea5d424e5815e149efdf7
Martin Helfer <martin.helfer@ub.uni-freiburg.de>
2024-11-20 16:18:12 +0100

    delete unused read wav files (chatGPT)

--- createReadFiles.js
+++ createReadFiles.js
@@ -30,10 +30,10 @@ for (const jsonFile of jsonFiles) {
     //TODO: Autorälli, etc.
     const titleToRead = jsonObj.name.replace(/ \- \d+ \-/, "");
     //console.log(titleToRead);
-    
+
     //Linux Version
     const lang = jsonObj.lang || "de-DE";
-    
+
     //Windows Version
     //TODO: Sprache englisch, spanisch aus jsonObj
     //const lang = "german";
@@ -54,7 +54,7 @@ for (const jsonFile of jsonFiles) {
                                 ffmpeg -i ${__dirname}/tts.wav -af equalizer=f=300:t=h:width=200:g=-30 ${__dirname}/tts-eq.wav -hide_banner -loglevel error -y &&
                                 ffmpeg -i ${__dirname}/tts-eq.wav -af acompressor=threshold=-11dB:ratio=9:attack=200:release=1000:makeup=8 "${readFilesDir}/${filename}" -hide_banner -loglevel error -y`;
       execSync(pico2waveTTScommand);
-      
+
       //Windows Version
       //execSync('balcon -t "' + titleToRead + '" -l ' + lang + ' -w tts.wav');
       //execSync('ffmpeg -i tts.wav -af equalizer=f=300:t=h:width=200:g=-30 tts-eq.wav -hide_banner -loglevel error -y');
@@ -62,4 +62,36 @@ for (const jsonFile of jsonFiles) {
     }
   }
 }
-console.log("Update printed list");
+
+// Find all wav files in the directory
+const allWavFiles = glob.sync(readFilesDir + "/*.wav").map((filePath) => path.basename(filePath));
+
+// Collect all filenames expected based on JSON files
+const expectedWavFiles = [];
+for (const jsonFile of jsonFiles) {
+  const topFolder = path.basename(path.dirname(jsonFile));
+  const subFolder = path.basename(jsonFile, ".json");
+  const jsonData = fs.readJsonSync(jsonFile);
+
+  for (const jsonObj of jsonData) {
+    const filename = `${topFolder}-${subFolder}-${jsonObj.file}.wav`;
+    expectedWavFiles.push(filename);
+  }
+}
+
+// Find orphaned wav files (those not listed in expectedWavFiles)
+const expectedSet = new Set(expectedWavFiles); // Use Set for faster comparison
+const orphanedWavFiles = allWavFiles.filter((wavFile) => !expectedSet.has(wavFile));
+
+// Delete orphaned files
+for (const orphanedFile of orphanedWavFiles) {
+  const fullPath = path.join(readFilesDir, orphanedFile);
+  try {
+    fs.unlinkSync(fullPath); // Deletes the file
+    console.log(`Deleted unused file: ${orphanedFile}`);
+  } catch (error) {
+    console.error(`Failed to delete file: ${orphanedFile}`, error);
+  }
+}
+
+//console.log("Cleanup complete. Removed all unused .wav files.");
