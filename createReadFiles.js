dedff4ecd4f1c9fa8feef20cde11ee63aa20c47f
Martin Helfer <martin.helfer@ub.uni-freiburg.de>
2025-02-12 10:15:49 +0100

    gpt update

--- createReadFiles.js
+++ createReadFiles.js
@@ -1,67 +1,45 @@
-//Vorlesedateien erzeugen fuer stt und random Joker
+// Vorlesedateien erzeugen fuer stt und random Joker
 
-//libraries laden fuer Dateizugriff
+// Libraries laden fuer Dateizugriff
 const fs = require("fs-extra");
 const glob = require("glob");
 const path = require("path");
 const execSync = require("child_process").execSync;
 
-//Pfade wo die Dateien lokal liegen
+// Pfade wo die Dateien lokal liegen
 const audioDir = fs.readJSONSync("config.json").audioDir;
-const readFilesDir = audioDir + "/wap/wav";
-const jsonDir = audioDir + "/wap/json/";
+const readFilesDir = `${audioDir}/wap/wav`;
+const jsonDir = `${audioDir}/wap/json`;
 
-//Audio-Infos aus JSON-Config-Dateien sammeln
-const jsonFiles = glob.sync(jsonDir + "/*/*.json");
-for (const jsonFile of jsonFiles) {
-  //hsp
+// Audio-Infos aus JSON-Config-Dateien sammeln
+const jsonFiles = glob.sync(`${jsonDir}/*/*.json`);
+
+// Process each JSON file
+jsonFiles.forEach(jsonFile => {
   const topFolder = path.basename(path.dirname(jsonFile));
-
-  //bobo
   const subFolder = path.basename(jsonFile, ".json");
 
-  //Ueber Playlists in bob.json gehen
+  // Read and parse JSON data
   const jsonData = fs.readJsonSync(jsonFile);
-  for (const jsonObj of jsonData) {
-    //hsp-bobo-kindergarten.wav
-    const filename = topFolder + "-" + subFolder + "-" + jsonObj.file + ".wav";
 
-    //Bobo Siebenschläfer - Willkommen im Kindergarten
-    //TODO: Autorälli, etc.
+  // Process each entry in the JSON data
+  jsonData.forEach(jsonObj => {
+    const filename = `${topFolder}-${subFolder}-${jsonObj.file}.wav`;
     const titleToRead = jsonObj.name.replace(/ \- \d+ \-/, "");
-    //console.log(titleToRead);
-
-    //Linux Version
     const lang = jsonObj.lang || "de-DE";
 
-    //Windows Version
-    //TODO: Sprache englisch, spanisch aus jsonObj
-    //const lang = "german";
-
-    //pico2wave - l ${ lang } -w ${ __dirname } /tts.wav "${titleToRead}" &&
-    //ffmpeg - i ${ __dirname } /tts.wav -af equalizer=f=300:t=h:width=200:g=-30 ${__dirname}/tts - eq.wav - hide_banner - loglevel error - y &&
-    //ffmpeg - i ${ __dirname } /tts-eq.wav -af acompressor=threshold=-11dB:ratio=9:attack=200:release=1000:makeup=8 "${readFilesDir}/${ filename } " -hide_banner -loglevel error -y`;
-
-    //@MH: Maerz 2023: balcon fuer TTS mit Windows
-    //Fehlende Sprachdatei berechnen, normalisieren und in Nextcloud ablegen
-
-    if (!fs.existsSync(readFilesDir + "/" + filename)) {
-      console.log("create " + filename);
-
-      //Linux Version
+    // Check if the file already exists
+    if (!fs.existsSync(`${readFilesDir}/${filename}`)) {
+      console.log(`create ${filename}`);
       const pico2waveTTScommand = `
-                                pico2wave -l ${lang} -w ${__dirname}/tts.wav "${titleToRead}" &&
-                                ffmpeg -i ${__dirname}/tts.wav -af equalizer=f=300:t=h:width=200:g=-30 ${__dirname}/tts-eq.wav -hide_banner -loglevel error -y &&
-                                ffmpeg -i ${__dirname}/tts-eq.wav -af acompressor=threshold=-11dB:ratio=9:attack=200:release=1000:makeup=8 "${readFilesDir}/${filename}" -hide_banner -loglevel error -y`;
+        pico2wave -l ${lang} -w ${__dirname}/tts.wav "${titleToRead}" &&
+        ffmpeg -i ${__dirname}/tts.wav -af equalizer=f=300:t=h:width=200:g=-30 ${__dirname}/tts-eq.wav -hide_banner -loglevel error -y &&
+        ffmpeg -i ${__dirname}/tts-eq.wav -af acompressor=threshold=-11dB:ratio=9:attack=200:release=1000:makeup=8 "${readFilesDir}/${filename}" -hide_banner -loglevel error -y
+      `;
       execSync(pico2waveTTScommand);
-
-      //Windows Version
-      //execSync('balcon -t "' + titleToRead + '" -l ' + lang + ' -w tts.wav');
-      //execSync('ffmpeg -i tts.wav -af equalizer=f=300:t=h:width=200:g=-30 tts-eq.wav -hide_banner -loglevel error -y');
-      //execSync('ffmpeg -i tts-eq.wav -af acompressor=threshold=-11dB:ratio=9:attack=200:release=1000:makeup=8 "' + readFilesDir + '/' + filename + '" -hide_banner -loglevel error -y');
     }
-  }
-}
+  });
+});
 
 // Find all wav files in the directory
 const allWavFiles = glob.sync(readFilesDir + "/*.wav").map((filePath) => path.basename(filePath));
